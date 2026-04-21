030f40585d279fcaf803f1ca3bbff4d7105b290b
jodantuene13 <76019479+jodantuene13@users.noreply.github.com>
2026-04-21 16:36:41 -0300

    OK

--- frontend/src/app/shared/components/ficha-producto-modal/ficha-producto-modal.component.ts
+++ frontend/src/app/shared/components/ficha-producto-modal/ficha-producto-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductoService } from '../../../services/producto.service';
 
 @Component({
   selector: 'app-ficha-producto-modal',
@@ -8,9 +9,31 @@ import { CommonModule } from '@angular/common';
   templateUrl: './ficha-producto-modal.component.html',
   styleUrls: []
 })
-export class FichaProductoModalComponent {
+export class FichaProductoModalComponent implements OnInit {
   @Input() stock: any; // Se acepta 'any' para soportar los distintos formatos (model Stock vs listado dropdown) que manejan la info central.
   @Output() close = new EventEmitter<void>();
+
+  isLoadingAtributos: boolean = true;
+
+  constructor(private productoService: ProductoService) {}
+
+  ngOnInit(): void {
+    if (this.stock && this.stock.idProducto) {
+      this.productoService.getById(this.stock.idProducto).subscribe({
+        next: (prod) => {
+          this.stock.atributos = prod.atributos || [];
+          this.stock.estadoProducto = prod.activo;
+          this.stock.familiaProducto = prod.nombreFamilia || this.stock.familiaProducto;
+          this.isLoadingAtributos = false;
+        },
+        error: () => {
+          this.isLoadingAtributos = false;
+        }
+      });
+    } else {
+      this.isLoadingAtributos = false;
+    }
+  }
 
   closeModal(): void {
     this.close.emit();

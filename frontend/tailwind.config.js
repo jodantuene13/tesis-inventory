7711154467f866a2afe24960670d111d6d71765c
Lucas Abdenur <abdenurlucas@gmail.com>
2026-06-23 18:08:34 -0300

    prueba cambios

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
         "./src/**/*.{html,ts}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gray: {
+                    50: 'var(--color-gray-50)',
+                    100: 'var(--color-gray-100)',
+                    200: 'var(--color-gray-200)',
+                    300: 'var(--color-gray-300)',
+                    400: 'var(--color-gray-400)',
+                    500: 'var(--color-gray-500)',
+                    600: 'var(--color-gray-600)',
+                    700: 'var(--color-gray-700)',
+                    800: 'var(--color-gray-800)',
+                    900: 'var(--color-gray-900)'
+                }
+            }
+        },
     },
     plugins: [],
 }
